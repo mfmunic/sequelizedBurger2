f73f1b7bf59749d5b6a52d20c13b8872d4374c37
mfmunic <mfmunic@gmail.com>
2017-07-30 11:06:34 -0700

    stuff

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,27 @@
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
 // Dependencies
+var mysql = require('mysql2');
 var Sequelize = require("sequelize");
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("burgers2_db", "root", "Mamumysql!", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+if (process.env.JAWSDB_URL){
+	var sequelize = new Sequelize(process.env.JAWSDB_URL, {
+	  logging: false,
+	  dialectOptions: {
+	    ssl: true /* for SSL config since Heroku gives you this out of the box */
+	  }
+	});
+} else {
+
+	var sequelize = new Sequelize("burgers2_db", "root", "Mamumysql!", {
+	  host: "localhost",
+	  dialect: "mysql",
+	  pool: {
+	    max: 5,
+	    min: 0,
+	    idle: 10000
+	  }
+	});
+}
 // Exports the connection for other files to use
 module.exports = sequelize;
